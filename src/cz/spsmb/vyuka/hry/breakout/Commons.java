9caf76160714acf284fa01bbf8ab7c946a2fc830
stemb <stemberk@spsmb.cz>
2021-11-23 11:12:08 +0100

    breakout +

---
+++ src/cz/spsmb/vyuka/hry/breakout/Commons.java
@@ -0,0 +1,15 @@
+package cz.spsmb.vyuka.hry.breakout;
+
+public interface Commons {
+
+    final int WIDTH = 300;
+    final int HEIGHT = 400;
+    final int BOTTOM_EDGE = 390;
+    final int N_OF_BRICKS = 30;
+    final int INIT_PADDLE_X = 200;
+    final int INIT_PADDLE_Y = 360;
+    //final int INIT_PADDLE_Y = 330;
+    final int INIT_BALL_X = 230;
+    final int INIT_BALL_Y = 355;
+    final int PERIOD = 10;
+}
