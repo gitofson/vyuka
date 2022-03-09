fa2843085a923b0319146f13fad9f291f8b6802f
stemberk <stemberk@spsmb.cz>
2022-03-09 11:15:14 +0100

    Piskorky pokracovani

---
+++ src/Pokus.java
@@ -0,0 +1,16 @@
+package src;
+
+public class Pokus {
+    public static void main(String[] args) {
+        System.out.println("Ahoj");
+        int n = 20;
+        int m = 20;
+        System.out.println("Soucet m+n je: " + (n+m));
+        System.out.println(n+m);
+        for (int i = 0; i < 10; i++) {
+            System.out.print(i+ ", ");
+        }
+        System.out.println();
+
+    }
+}
